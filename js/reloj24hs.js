6997d5daad6ff948eb8d301cbe36129a3437baa0
Mauricio-Alonso <conta.alonso@gmail.com>
2022-08-17 16:44:53 -0300

    Ejercicio listo para merge con master.

--- js/reloj24hs.js
+++ js/reloj24hs.js
@@ -27,11 +27,39 @@ function obtenerHora(){
 setInterval(obtenerHora,1000);
 
 
+
+let IdiaNoche = document.getElementById('diaNoche')
+let fechaActualDiaNoche = new Date();
+let horasDiaNoche = fechaActualDiaNoche.getHours();
+console.log(horasDiaNoche)
+if(horasDiaNoche >=7 && horasDiaNoche <19){
+    IdiaNoche.classList.add("fa-solid");
+    IdiaNoche.classList.add("fa-sun");
+    IdiaNoche.classList.add("fs-1");
+    IdiaNoche.classList.add("titulo");
+    IdiaNoche.classList.add("my-4");
+    IdiaNoche.classList.add("fondoAzul");
+
+}else{
+    IdiaNoche.classList.add("fa-solid");
+    IdiaNoche.classList.add("fa-moon");
+    IdiaNoche.classList.add("fs-1");
+    IdiaNoche.classList.add("titulo");
+    IdiaNoche.classList.add("my-4");
+    IdiaNoche.classList.add("fondoAzul");
+
+}
+
+
+
 let t1Azul = document.getElementById('t1')
 function cambiarAzul(){
 t1Azul.classList.remove("fondoRosa");
 t1Azul.classList.remove("fondoVerde");
 t1Azul.classList.add("fondoAzul");
+IdiaNoche.classList.remove("fondoRosa");
+IdiaNoche.classList.remove("fondoVerde");
+IdiaNoche.classList.add("fondoAzul");
 }  
 
 
@@ -40,6 +68,9 @@ function cambiarVerde(){
 t1Verde.classList.remove("fondoAzul");
 t1Verde.classList.remove("fondoRosa");
 t1Verde.classList.add("fondoVerde");
+IdiaNoche.classList.remove("fondoAzul");
+IdiaNoche.classList.remove("fondoRosa");
+IdiaNoche.classList.add("fondoVerde");
 }   
 
 let t1Rosa = document.getElementById('t1')
@@ -47,4 +78,7 @@ function cambiarRosa(){
 t1Rosa.classList.remove("fondoVerde");
 t1Rosa.classList.remove("fondoAzul");
 t1Rosa.classList.add("fondoRosa");
+IdiaNoche.classList.remove("fondoVerde");
+IdiaNoche.classList.remove("fondoAzul");
+IdiaNoche.classList.add("fondoRosa");
 }  
